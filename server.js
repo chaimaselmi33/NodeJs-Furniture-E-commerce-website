e76a4f81e9c07c48395961864b8a6e2381a3df3b
chaimaselmi <chaima.selmi@sesame.com.tn>
2023-01-02 15:11:21 +0100

    updated

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParse = require('body-parser')
 const path = require('path')
 const exp = require('constants')
+const { send } = require('process')
 const port = 4200
 const app = express()
 
@@ -17,8 +18,9 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(bodyParse.json());
 app.use(bodyParse.urlencoded());
 
-app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use(express.static('public'));
+app.use('/css', express.static(path.join(__dirname, 'node_modules' ,'bootstrap' ,'dist' ,'css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules' ,'bootstrap' ,'dist' ,'js')))
 
 
 var router = require('./routes/pages')
