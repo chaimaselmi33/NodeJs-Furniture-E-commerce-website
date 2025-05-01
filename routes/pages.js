5cc049275a8960a88d58d82d979ad038a6e7a69d
chaima selmi <chaima.selmi@sesame.com.tn>
2025-05-01 17:43:44 +0200

    Enhancements

--- routes/pages.js
+++ routes/pages.js
@@ -1,71 +1,63 @@
-const express = require('express')
-const dbConnection = require('../database/config2')
+const express = require("express");
+const dbConnection = require("../database/config");
 
-const router = express.Router()
+const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render('home-page')
-})
+  res.render("home-page");
+});
 
 router.get("/products", (req, res) => {
-    let sql = "select * from furniture"
-    dbConnection.query(sql, (err, result) => {
-        if (err) console.log(err)
-        //The Buffer class in Node. js is designed to handle raw binary data. Each buffer corresponds to some raw memory allocated outside V8. Buffers act somewhat like arrays of integers, but aren't resizable and have a whole bunch of methods specifically for binary data
-        //---------------------------------------------
-        else {
-            result.forEach(frn => {
-                var b64 = frn.furn_image.toString('base64');
-                var image = "data:image/jpeg;base64," + b64
-                frn.furn_image = image
-            });
+  let sql = "select * from furniture";
+  dbConnection.query(sql, (err, result) => {
+    if (err) console.log(err);
+    else {
+      result.forEach((frn) => {
+        var b64 = frn.furn_image.toString("base64");
+        var image = "data:image/jpeg;base64," + b64;
+        frn.furn_image = image;
+      });
 
-            res.render('products-page', {
-                products: result
-            })
-        }
-    })
-})
+      res.render("products-page", {
+        products: result,
+      });
+    }
+  });
+});
 
-var qte = 1
+var qte = 1;
 router.get("/products/:id", (req, res) => {
-    const prod_id = req.params.id
-    let sql = `select * from furniture where furn_id = ${prod_id}`
-    dbConnection.query(sql, (err, result) => {
-
-        if (err) console.log(err)
-        else {
-            var b64 = result[0].furn_image.toString('base64')
-            var image = "data:image/jpeg;base64," + b64
-            res.render('productDetails-page', {
-                product: result[0],
-                image : image,
-                image2 : "./green.jpg",
-                qte:qte
-            })
-        }
-
-    })
-
-})
+  const prod_id = req.params.id;
+  let sql = `select * from furniture where furn_id = ${prod_id}`;
+  dbConnection.query(sql, (err, result) => {
+    if (err) console.log(err);
+    else {
+      var b64 = result[0].furn_image.toString("base64");
+      var image = "data:image/jpeg;base64," + b64;
+      res.render("productDetails-page", {
+        product: result[0],
+        image: image,
+        image2: "./green.jpg",
+        qte: qte,
+      });
+    }
+  });
+});
 
 router.get("/contact", (req, res) => {
-    res.render('contact-page')
-})
+  res.render("contact-page");
+});
 
 router.post("/postform", (req, res) => {
-    data = req.body
-    sql = "insert into contact set ?"
+  data = req.body;
+  sql = "insert into contact set ?";
 
-    dbConnection.query(sql, data, (err) => {
-        if (err) console.log(err)
-        console.log("data sent")
-    })
-})
+  dbConnection.query(sql, data, (err) => {
+    if (err) {
+      console.log("server error", err);
+    }
+    res.redirect("/contact");
+  });
+});
 
-module.exports = router
-
-
-/*const buffer = result[0].furn_image;
-        const b64 = buffer.toString('base64');
-        var image = "data:image/jpeg;base64," + b64*/
+module.exports = router;
